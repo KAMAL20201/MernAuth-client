96dc49270ba3ff660e44b80beba343e08694ece3
KAMAL20201 <arorakamal706@gmail.com>
2024-05-10 23:49:13 +0530

    Fix: used cookie for private route instead of currentUser

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { getCookie } from "../../utils/constants";
 
 const PrivateRoute = ({ component: Component }) => {
-  const { currentUser } = useSelector((state) => state.user);
+  const accessTokenCookie = getCookie("access_token");
 
-  return currentUser ? <Component /> : <Navigate to="/sign-in" />;
+  return accessTokenCookie ? <Component /> : <Navigate to="/sign-in" />;
 };
 
 export default PrivateRoute;
